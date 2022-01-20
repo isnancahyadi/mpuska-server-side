940855cef8b527abc004b805d0f8d5d710625667
isnancahyadi <isnan.arifc@gmail.com>
2022-01-20 11:37:19 +0700

    setting awal web admin

---
+++ mpuska-server/app/Views/home.php
@@ -0,0 +1,17 @@
+<?= $this->extend('layout/default') ?>
+
+<?= $this->section('title') ?>
+<title>Home &mdash; Mpuska</title>
+<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+<section class="section">
+    <div class="section-header">
+        <h1>Dashboard</h1>
+    </div>
+
+    <div class="section-body">
+        Hello World!
+    </div>
+</section>
+<?= $this->endSection() ?>
