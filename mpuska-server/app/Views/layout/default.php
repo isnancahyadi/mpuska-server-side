9c2b5bd8d703b5bb8b1aff12216edc8cc47cee0a
isnancahyadi <isnan.arifc@gmail.com>
2022-01-27 08:34:38 +0700

    tambah tanggal

--- mpuska-server/app/Views/layout/default.php
+++ mpuska-server/app/Views/layout/default.php
@@ -25,9 +25,11 @@
                 <form class="form-inline mr-auto">
                     <ul class="navbar-nav mr-3">
                         <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
-                        <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
+                        <!-- <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li> -->
                     </ul>
-                    <div class="search-element">
+                    <?php date_default_timezone_set('Asia/Jakarta') ?>
+                    <div class="navbar-text"><?= date('d F Y') ?></div>
+                    <!-- <div class="search-element">
                         <input class="form-control" type="search" placeholder="Search" aria-label="Search" data-width="250">
                         <button class="btn" type="submit"><i class="fas fa-search"></i></button>
                         <div class="search-backdrop"></div>
@@ -88,7 +90,7 @@
                                 </a>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                 </form>
                 <ul class="navbar-nav navbar-right">
                     <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link nav-link-lg message-toggle beep"><i class="far fa-envelope"></i></a>
