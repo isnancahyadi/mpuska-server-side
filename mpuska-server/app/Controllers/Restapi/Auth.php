6e829e2bc8a3d82bf4e6256f763c299e5c2a52e5
isnancahyadi <isnan.arifc@gmail,com>
2022-03-10 14:31:23 +0700

    menambahkan API login dan logout

---
+++ mpuska-server/app/Controllers/Restapi/Auth.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controllers\Restapi;
+
+use App\Controllers\BaseController;
+use CodeIgniter\API\ResponseTrait;
+
+class Auth extends BaseController
+{
+    use ResponseTrait;
+
+    public function index()
+    {
+        //
+    }
+
+    public function loginProcess()
+    {
+        $post = $this->request->getPost();
+        $query = $this->db->table('akun')->getWhere(['username' => $post['username']]);
+        $user = $query->getRow();
+
+        if ($post['username'] == null) {
+            return $this->fail('Silahkan isi username dan password');
+        } else {
+            if ($user) {
+                if (password_verify($post['password'], $user->password)) {
+                    $params = [
+                        'ID_akun' => $user->ID_akun,
+                        'username' => $user->username,
+                        'hak_akses' => $user->hak_akses
+                    ];
+                    session()->set($params);
+
+                    return $this->respond($params, 200);
+                } else {
+                    return $this->failValidationErrors('NIY/NIM atau password salah');
+                }
+            } else {
+                return $this->failNotFound('NIY/NIM tidak ditemukan');
+            }
+        }
+    }
+
+    public function logoutProcess()
+    {
+        session()->remove('ID_akun');
+    }
+}
